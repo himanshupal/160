2d3df90f3cea9379a74b9bad845c9eaafeb7a220
Himanshu Pal <mailtohimanshupal@gmail.com>
2023-07-29 19:23:59 +0530

    Added other selectors

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,5 +1,24 @@
 /** Name of days to show on calendar */
 export const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
+/** Name of months to show on calendar */
+export const months = [
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec'
+];
+
 /** Offset 0 is treated as Sunday */
 export const dayOffset = 2;
+
+/** No. of years that can be scrolled in past & future */
+export const yearsRange = 100;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { dayOffset, days } from '$lib/config';
+	import { dayOffset, days, months, yearsRange } from '$lib/config';
+	import downIcon from '$lib/icons/down.svg';
+	import upIcon from '$lib/icons/up.svg';
 	import dayjs from 'dayjs';
 
 	interface IDateValue {
@@ -7,24 +9,65 @@
 		value: number;
 	}
 
+	interface IMonthValue extends IDateValue {
+		text: string;
+	}
+
 	const d = dayjs();
 
 	let slicedDateList: IDateValue[][];
+	let slicedYearList: IDateValue[][];
 
+	let selectionStage: 0 | 1 | 2 = 0;
 	let currentMonth = d.month();
 	let currentYear = d.year();
 	let datePickerOpen = true;
+
+	const monthsList: IMonthValue[] = [
+		...months.map((value, i) => ({ active: true, value: i, text: value })),
+		...months.slice(0, 4).map((value, i) => ({ active: false, value: i, text: value }))
+	];
 
 	$: {
 		const yearDiff = Math.floor((currentMonth - 12) / 12) + 1;
 		currentYear = d.year(d.year() + yearDiff).year();
 	}
 
+	$: farthestYearInPast = d.year() - yearsRange;
 	$: displayMonth = d.month(currentMonth).month() + 1;
-	$: daysInMonth = d.month(currentMonth).daysInMonth();
 	$: weekdays = dayOffset ? [...days.slice(dayOffset), ...days.slice(0, dayOffset)] : days;
+	$: slicedMonthList = Array.from({ length: 4 }, (_, i) => monthsList.slice(i * 4, i * 4 + 4));
+
+	$: decadeRange = (() => {
+		let year = currentYear % 100;
+		year -= year % 10;
+		let startOfDecade = +`${Math.floor(currentYear / 100)}${String(year).padEnd(2, '0')}`;
+		if (startOfDecade < farthestYearInPast) startOfDecade = farthestYearInPast;
+		return { start: startOfDecade, end: startOfDecade + 9 };
+	})();
 
 	$: {
+		const { start, end } = decadeRange;
+		let startIndex = (start - farthestYearInPast) % 4;
+		let yearsList: IDateValue[] = [
+			...Array.from({ length: startIndex }, (_, i) => ({
+				active: false,
+				value: start - (i + 1)
+			})).reverse(),
+			...Array.from({ length: 10 }, (_, i) => ({ active: true, value: start + i }))
+		];
+		yearsList = [
+			...yearsList,
+			...Array.from({ length: 16 - yearsList.length }, (_, i) => ({
+				active: false,
+				value: end + (i + 1)
+			}))
+		];
+		slicedYearList = Array.from({ length: 4 }, (_, i) => yearsList.slice(i * 4, i * 4 + 4));
+	}
+
+	$: {
+		const daysInMonth = d.month(currentMonth).daysInMonth();
 		const startAt = d.month(currentMonth).set('date', 1).day();
 		const daysInPreviousMonth = d.month(currentMonth - 1).daysInMonth();
 		let dateList: IDateValue[] = [
@@ -52,16 +95,34 @@
 		slicedDateList = Array.from({ length: 6 }, (_, i) => dateList.slice(i * 7, i * 7 + 7));
 	}
 
-	const toggleDatePicker = () => {
-		datePickerOpen = !datePickerOpen;
-	};
+	const toggleDatePicker = () => (datePickerOpen = !datePickerOpen);
 
 	const previous = () => {
-		currentMonth -= 1;
+		switch (selectionStage) {
+			case 0:
+				return (currentMonth -= 1);
+			case 1:
+				return (currentMonth -= 12);
+			case 2:
+				return (currentMonth -= 120);
+		}
 	};
 
 	const next = () => {
-		currentMonth += 1;
+		switch (selectionStage) {
+			case 0:
+				return (currentMonth += 1);
+			case 1:
+				return (currentMonth += 12);
+			case 2:
+				return (currentMonth += 120);
+		}
+	};
+
+	const updateSelectionStage = () => {
+		if (selectionStage < 2) {
+			selectionStage += 1;
+		}
 	};
 </script>
 
@@ -86,28 +147,64 @@
 		{#if datePickerOpen}
 			<div class="datepicker">
 				<div class="datepicker__actions">
-					<span>{displayMonth} {currentYear}</span>
+					<button class="datepicker__actions__selector" on:click={updateSelectionStage}>
+						{#if !selectionStage}
+							{displayMonth}
+						{/if}
+						{selectionStage === 2 ? `${decadeRange.start} - ${decadeRange.end}` : currentYear}
+					</button>
+
 					<div class="datepicker__actions__buttons">
-						<button on:click={previous}>&lt;</button>
-						<button on:click={next}>&gt;</button>
+						<button
+							on:click={previous}
+							disabled={farthestYearInPast === slicedYearList[0][0].value}
+						>
+							<img src={upIcon} alt="<" width="24px" height="24px" />
+						</button>
+						<button on:click={next}>
+							<img src={downIcon} alt=">" width="24px" height="24px" />
+						</button>
 					</div>
 				</div>
-				<div class="datepicker__row">
-					{#each weekdays as day}
-						<div class="datepicker__cell week">
-							{day}
-						</div>
-					{/each}
-				</div>
-				{#each slicedDateList as week}
+
+				{#if !selectionStage}
 					<div class="datepicker__row">
-						{#each week as date}
-							<div class="datepicker__cell" class:active={date.active}>
-								{date.value}
+						{#each weekdays as day}
+							<div class="datepicker__cell nohover">
+								{day}
 							</div>
 						{/each}
 					</div>
-				{/each}
+					{#each slicedDateList as week}
+						<div class="datepicker__row">
+							{#each week as date}
+								<div class="datepicker__cell" class:active={date.active}>
+									{date.value}
+								</div>
+							{/each}
+						</div>
+					{/each}
+				{:else if selectionStage === 1}
+					{#each slicedMonthList as monthList}
+						<div class="datepicker__row">
+							{#each monthList as month}
+								<div class="datepicker__cell lg" class:active={month.active}>
+									{month.text}
+								</div>
+							{/each}
+						</div>
+					{/each}
+				{:else if selectionStage === 2}
+					{#each slicedYearList as yearList}
+						<div class="datepicker__row">
+							{#each yearList as year}
+								<div class="datepicker__cell lg" class:active={year.active}>
+									{year.value}
+								</div>
+							{/each}
+						</div>
+					{/each}
+				{/if}
 			</div>
 		{/if}
 	</form>
@@ -119,12 +216,12 @@
 	$text-color-secondary: #686868;
 
 	.container {
-		display: flex;
 		width: 100dvw;
-		min-height: 100dvh;
+		height: 65dvh;
 
-		display: grid;
-		place-content: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
 	.content {
@@ -142,6 +239,7 @@
 		border: none;
 		outline: none;
 		width: 100%;
+		flex: 2;
 	}
 
 	.datepicker {
@@ -150,6 +248,7 @@
 		left: 50%;
 		transform: translateX(-50%);
 
+		gap: 0.25rem;
 		display: flex;
 		flex-direction: column;
 
@@ -158,19 +257,52 @@
 
 		color: $text-color-primary;
 		padding: 0.5rem;
-		border-radius: 0.25rem;
+		border-radius: 0.35rem;
 		border: 2px solid lighten($background-primary, 2.5%);
 		background-color: darken($background-primary, 3%);
 
 		&__actions {
+			gap: 0.5rem;
 			display: flex;
-			align-items: center;
+			align-items: stretch;
 			justify-content: space-between;
-			padding: 0.5rem;
+
+			&__selector {
+				background-color: darken($background-primary, 3%);
+				transition: all 75ms ease-in-out;
+				border-radius: 0.35rem;
+				font-family: inherit;
+				font-size: medium;
+				text-align: left;
+				font-weight: 600;
+				cursor: pointer;
+				padding: 0.5rem;
+				color: inherit;
+				border: unset;
+				flex: 1;
+
+				&:hover {
+					background-color: lighten($background-primary, 3%);
+				}
+			}
 
 			&__buttons {
+				align-items: stretch;
 				display: flex;
-				gap: 1rem;
+				gap: 0.5rem;
+
+				> button {
+					border: none;
+					padding: unset;
+					cursor: pointer;
+					background-color: transparent;
+					transition: all 75ms ease-in-out;
+					border-radius: 0.35rem;
+
+					&:hover {
+						background-color: lighten($background-primary, 3%);
+					}
+				}
 			}
 		}
 
@@ -188,7 +320,17 @@
 			place-content: center;
 			transition: all 75ms ease-in-out;
 
-			&:not(&.week) {
+			&.nohover {
+				color: darken($text-color-primary, 25%);
+				font-weight: 600;
+			}
+
+			&.lg {
+				width: 3.688rem;
+				height: 3.688rem;
+			}
+
+			&:not(&.nohover) {
 				cursor: pointer;
 
 				&:hover {
